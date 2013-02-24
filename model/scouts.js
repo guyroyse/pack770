309bbd133b344e051a1d27f3e1097bcd19fc20ba
Guy Royse <guy@guyroyse.com>
2013-02-24 16:15:24 -0500

    Fixed error in active status display, model now activates/deactivies

--- model/scouts.js
+++ model/scouts.js
@@ -9,6 +9,14 @@ Pack770.Model.Scouts.all = function() {
   return Scouts.find({}, { sort: { firstName: 1, lastName: 1 } });
 };
 
+Pack770.Model.Scouts.activate = function(scout) {
+  Scouts.update({_id: scout}, {$set: {active: true}});
+};
+
+Pack770.Model.Scouts.deactivate = function(scout) {
+  Scouts.update({_id: scout}, {$set: {active: false}});
+};
+
 Pack770.Model.Scouts.insert = function(scout) {
   scout.active = true;
   Scouts.insert(scout);  
